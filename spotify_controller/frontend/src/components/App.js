ba5744defa6adaebc2cbeddae8ac36c5cb8cda04
Brian Connolly <Brian.connolly71@gmail.com>
2022-04-27 18:09:25 +0100

    got setup webpage running

--- spotify_controller/frontend/src/components/App.js
+++ spotify_controller/frontend/src/components/App.js
@@ -5,4 +5,11 @@ export default class App extends Component {
     constructor(props) {
         super(props);
     }
+
+    render() {
+        return <h1>Hey Guys!</h1>
+    }
 }
+
+const appDiv = document.getElementById('app');
+render(<App />, appDiv);
