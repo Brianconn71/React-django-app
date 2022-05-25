b451709f108193db114f0a75f5140b9eed4ffdcc
Brian Connolly <Brian.connolly71@gmail.com>
2022-05-25 18:16:04 +0100

    working on api response from backend

--- spotify_controller/frontend/src/components/CreateRoomPage.js
+++ spotify_controller/frontend/src/components/CreateRoomPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
@@ -11,70 +11,105 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import { Link } from "react-router-dom";
 
 export default class CreateRoomPage extends Component {
-    defaultVotes = 2;
-    constructor(props){
-        super(props);
-        this.state = {
-            guestCanPause: true,
-            votesToSkip: this.defaultVotes,
-        };
-    }
+  defaultVotes = 2;
+  constructor(props) {
+    super(props);
+    this.state = {
+      guestCanPause: true,
+      votesToSkip: this.defaultVotes,
+    };
 
-    handleVotesChange(e){
-        this.setState({
-            votesToSkip: e.target.value,
-        });
-    }
+    this.handleRoomButton = this.handleRoomButton.bind(this);
+    this.handleVotesChange = this.handleVotesChange.bind(this);
+    this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
+  }
 
-    handleGuestCanPauseChange(e) {
-        this.setState({
-            guestCanPause: e.target.value === "true" ? true : false,
-        });
-    }
+  handleVotesChange(e) {
+    this.setState({
+      votesToSkip: e.target.value,
+    });
+  }
 
-    render (){
-        return( 
-        <Grid container spacing={1}>
-            <Grid item xs={12} align="center">
-                <Typography component='h4' variant='h4'>
-                    Create
-                </Typography>
-                <Typography component='h4' variant='h4'>
-                    <FormControl component="fieldset">
-                        <FormHelperText>
-                            <div align="center">ewsfjkhdlasfirgh</div>
-                        </FormHelperText>
-                    </FormControl>
-                    <RadioGroup row align="center" defaultValue="true">
-                        <FormControlLabel value="true" control={<Radio color="primary" />} label="Play/Pause" labelPlacement="bottom" />
-                        <FormControlLabel value="false" control={<Radio color="secondary" />} label="No control" labelPlacement="bottom" />
-                    </RadioGroup>
-                </Typography>
-            </Grid>
-            <Grid item xs={12} align="center">
-                <FormControl>
-                    <TextField required={true} type="number" defaultValue={this.defaultVotes} inputProps={{
-                        min: 1,
-                        style: { textAlign: "center" }
-                    }} />
-                    <FormHelperText>
-                        <div align="center">
-                            Votes required to skip.
-                        </div>
-                    </FormHelperText>
-                </FormControl>
-            </Grid>
-            <Grid item xs={12} align="center">
-                <Button color="primary" variant="contained">
-                    Create A Room
-                </Button>
-            </Grid>
-            <Grid item xs={12} align="center">
-                <Button color="secondary" variant="contained" to="/" component={Link}>
-                    Back
-                </Button>
-            </Grid>
+  handleGuestCanPauseChange(e) {
+    this.setState({
+      guestCanPause: e.target.value === "true" ? true : false,
+    });
+  }
+
+  handleRoomButton() {
+    const requestOptions = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            votesToSkip: this.state.votesToSkip,
+            guestCanPause: this.state.guestCanPause,
+        })
+    };
+    fetch('/api/create-room', requestOptions).then((response) => response.json())
+  }
+
+  render() {
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography component="h4" variant="h4">
+            Create
+          </Typography>
+          <Typography component="h4" variant="h4">
+            <FormControl component="fieldset">
+              <FormHelperText>
+                <div align="center">ewsfjkhdlasfirgh</div>
+              </FormHelperText>
+            </FormControl>
+            <RadioGroup
+              row
+              align="center"
+              defaultValue="true"
+              onChange={this.handleGuestCanPauseChange}
+            >
+              <FormControlLabel
+                value="true"
+                control={<Radio color="primary" />}
+                label="Play/Pause"
+                labelPlacement="bottom"
+              />
+              <FormControlLabel
+                value="false"
+                control={<Radio color="secondary" />}
+                label="No control"
+                labelPlacement="bottom"
+              />
+            </RadioGroup>
+          </Typography>
         </Grid>
-        );
-    }
+        <Grid item xs={12} align="center">
+          <FormControl>
+            <TextField
+              required={true}
+              onChange={this.handleVotesChange}
+              type="number"
+              defaultValue={this.defaultVotes}
+              inputProps={{
+                min: 1,
+                style: { textAlign: "center" },
+              }}
+            />
+            <FormHelperText>
+              <div align="center">Votes required to skip.</div>
+            </FormHelperText>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button color="primary" variant="contained" onClick={this.handleRoomButton}>
+            Create A Room
+          </Button>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button color="secondary" variant="contained" to="/" component={Link}>
+            Back
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
 }
